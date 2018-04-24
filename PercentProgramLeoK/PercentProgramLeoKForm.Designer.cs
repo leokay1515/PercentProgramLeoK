0a6c6d7f6d47c1717ab65ebe4a781c0dd9ad3fbd
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-24 13:55:27 -0400

    Initial commit

---
+++ PercentProgramLeoK/PercentProgramLeoKForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PercentProgramLeoK
+{
+    partial class frmPercentProgramLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.btnGetPercent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 36);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(215, 20);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Please enter a level mark:";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(233, 36);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(108, 20);
+            this.txtLevel.TabIndex = 1;
+            // 
+            // btnGetPercent
+            // 
+            this.btnGetPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetPercent.Location = new System.Drawing.Point(127, 91);
+            this.btnGetPercent.Name = "btnGetPercent";
+            this.btnGetPercent.Size = new System.Drawing.Size(123, 36);
+            this.btnGetPercent.TabIndex = 2;
+            this.btnGetPercent.Text = "Get Percent";
+            this.btnGetPercent.UseVisualStyleBackColor = true;
+            this.btnGetPercent.Click += new System.EventHandler(this.btnGetPercent_Click);
+            // 
+            // frmPercentProgramLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 169);
+            this.Controls.Add(this.btnGetPercent);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.lblRequest);
+            this.Name = "frmPercentProgramLeoK";
+            this.Text = "Percentage Program by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.Button btnGetPercent;
+    }
+}
+
---
+++ PercentProgramLeoK/PercentProgramLeoKForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PercentProgramLeoK
+{
+    public partial class frmPercentProgramLeoK : Form
+    {
+        public frmPercentProgramLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private int ConvertToPercent(String level)
+        {
+            //variable
+            int percent;
+
+            switch (level)
+            {
+                case "4++":
+                    percent = 98;
+                    break;
+                case "4+":
+                    percent = 95;
+                    break;
+                case "4":
+                    percent = 87;
+                    break;
+                case "4-":
+                    percent = 83;
+                    break;
+                case "3+":
+                    percent = 78;
+                    break;
+                case "3":
+                    percent = 75;
+                    break;
+                case "3-":
+                    percent = 72;
+                    break;
+                case "2+":
+                    percent = 68;
+                    break;
+                case "2":
+                    percent = 65;
+                    break;
+                case "2-":
+                    percent = 62;
+                    break;
+                case "1+":
+                    percent = 58;
+                    break;
+                case "1":
+                    percent = 55;
+                    break;
+                case "1-":
+                    percent = 52;
+                    break;
+                case "0+":
+                    percent = 45;
+                    break;
+                case "0":
+                    percent = 40;
+                    break;
+                case "0-":
+                    percent = 35;
+                    break;
+                case "0--":
+                    percent = 30;
+                    break;
+                case "0---":
+                    percent = 20;
+                    break;
+                case "0----":
+                    percent = 10;
+                    break;
+                default:
+                    percent = -1;
+                    break;
+            }
+
+            return percent;
+        }
+
+        private void btnGetPercent_Click(object sender, EventArgs e)
+        {
+            //variables
+            String userLevel;
+            int userPercent;
+
+            //get the level from the user
+            userLevel = txtLevel.Text;
+
+            //call the function that tells the user their percent
+            userPercent = ConvertToPercent(userLevel);
+
+            //display the percent
+            MessageBox.Show(userLevel + " is equal to " + userPercent + " percent.", "Level to Percent");
+        }
+    }
+}
